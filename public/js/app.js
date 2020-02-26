0b335a3f7988a3cca45cf031e5d818086d75ca93
Max Power <dbroncosfan@gmail.com>
2020-02-25 23:51:06 -0700

    updated routes, save/unsave route partially working, clear articles route working

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,43 @@
 $("#new_articles").on("click", function (event) {
     event.preventDefault();
-    $.get("/scrape", function(data) {
+    $.get("/scrape", function () {
         window.location.reload(true)
     });
+});
+
+$("#clear_articles").on("click", function (event) {
+    event.preventDefault();
+    $.get("/delete", function () {
+        window.location.reload(true)
+    });
+});
+
+$("#save_article").on("click", function () {
+    var saved_article = $(this).data();
+    saved_article.saved = true;
+
+    var id = $(this).attr("data-articleId");
+    $.ajax("/saved/" + id, {
+        type: "PUT",
+        data: saved_article
+    }).then(
+        function(data) {
+            location.reload();
+        }
+    );
+});
+
+$("#remove_article").on("click", function () {
+    var removed_article = $(this).data();
+    var id = $(this).attr("data-articleId");
+    removed_article.saved = false;
+
+    $.ajax("/saved/" + id, {
+        type: "PUT",
+        data: removed_article
+    }).then(
+        function(data) {
+            location.reload();
+        }
+    );
 });
